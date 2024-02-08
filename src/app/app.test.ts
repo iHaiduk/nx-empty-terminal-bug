c6765f442e0ca36758c61a678ab8376710e22ba8
Ihor Haiduk <cookkycok@gmail.com>
2024-02-08 09:41:47 +0200

    bug example

--- src/app/app.test.ts
+++ src/app/app.test.ts
@@ -1,13 +1,13 @@
 import 'expect-puppeteer';
 import puppeteer, { Page, Browser } from 'puppeteer';
 
-describe('Basic authentication e2e tests', () => {
+describe('Basic tests', () => {
   let browser: Browser;
   let page: Page;
   beforeAll(async () => {
     browser = await puppeteer.launch({
       headless: false,
-      slowMo: 50,
+      slowMo: 250,
     });
     page = await browser.newPage();
 
@@ -22,32 +22,3 @@ describe('Basic authentication e2e tests', () => {
     browser.close();
   });
 });
-
-// (async () => {
-//   // Launch the browser and open a new blank page
-//   // const browser = await puppeteer.launch();
-
-//
-//   // Navigate the page to a URL
-//   await page.goto('http://localhost:4200/');
-//
-//   // Set screen size
-//   await page.setViewport({ width: 1360, height: 1024 });
-//
-//   await page.waitForSelector(getTestIdQuery(LoginTestEnum.Email));
-//   await page.type(getTestIdQuery(LoginTestEnum.Email), 'Americo.Carter47@gmail.com');
-//   await page.waitForSelector(getTestIdQuery(LoginTestEnum.Password));
-//   await page.type(getTestIdQuery(LoginTestEnum.Email), 'jAdlctrLMGO6tfe');
-//
-//
-//   // await page.click(searchResultSelector);
-//   //
-//   // // Locate the full title with a unique string
-//   // const textSelector = await page.waitForSelector('text/Customize and automate');
-//   // const fullTitle = await textSelector?.evaluate((el) => el.textContent);
-//   //
-//   // // Print the full title
-//   // console.log('T2323 is "%s".', fullTitle);
-//
-//   await browser.close();
-// })();
